eabf59ba64acf71ec1ad191198b1860e5cd5d664
CarlosEReis <carlos.er7@gmail.com>
2025-05-07 11:39:12 -0300

    refactor: remove heranca do JpaRepository
     - Remove herança do Spring Data JPA (JpaRepository), e cria cria os contratos utilizados pela ClienteService

--- src/main/java/com/er7system/er7bank/domain/repository/ClienteRepository.java
+++ src/main/java/com/er7system/er7bank/domain/repository/ClienteRepository.java
@@ -1,8 +1,14 @@
 package com.er7system.er7bank.domain.repository;
 
 import com.er7system.er7bank.domain.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ClienteRepository {
+
+    Cliente save(Cliente cliente);
+    Optional<Cliente> findById(Long idCliente);
+    List<Cliente> findAll();
+    void deleteById(Long idCliente);
 }
