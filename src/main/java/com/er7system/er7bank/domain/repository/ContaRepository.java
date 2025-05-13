9f08880f6275c99a508feb1dec022eb8742c1996
CarlosEReis <carlos.er7@gmail.com>
2025-05-13 15:10:49 -0300

    refactor: remove heranca do JpaRepository
     - Remove herança do Spring Data JPA (JpaRepository), e cria cria os contratos utilizados pela ContaService

--- src/main/java/com/er7system/er7bank/domain/repository/ContaRepository.java
+++ src/main/java/com/er7system/er7bank/domain/repository/ContaRepository.java
@@ -1,12 +1,14 @@
 package com.er7system.er7bank.domain.repository;
 
 import com.er7system.er7bank.domain.model.Conta;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface ContaRepository extends JpaRepository<Conta, Integer> {
+public interface ContaRepository {
 
     List<Conta> findByClienteId(Long clienteId);
-
+    Conta save(Conta conta);
+    Optional<Conta> findById(Integer idConta);
+    List<Conta> findAll();
 }
