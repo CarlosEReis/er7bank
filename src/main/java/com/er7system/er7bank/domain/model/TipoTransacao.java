4c8cd52dca34a6c09cad28de456cd2e85fd35972
CarlosEReis <carlos.er7@gmail.com>
2025-05-15 16:20:09 -0300

    refactor: atualiza enum TipoTransacao
     - Atualiza enum TipoTransacao com novas transações, descrições e TipoOperacao

--- src/main/java/com/er7system/er7bank/domain/model/TipoTransacao.java
+++ src/main/java/com/er7system/er7bank/domain/model/TipoTransacao.java
@@ -2,12 +2,30 @@ package com.er7system.er7bank.domain.model;
 
 public enum TipoTransacao {
 
-    SAQUE,
-    DEPOSITO,
-    TRANSFERENCIA,
-    PIX,
-    PAGAMENTO,
-    CREDITO,
-    DEBITO;
+    TRANSFERENCIA_RECEBIDA("Transferência Recebida", TipoOperacao.ENTRADA),
+    TRANSFERENCIA_ENVIADA("Transferência Enviada", TipoOperacao.SAIDA),
+    DEPOSITO_CAIXA_ELETRONICO("Depósito no caixa eletrônico Realizado", TipoOperacao.ENTRADA),
+    SAQUE_CAIXA_ELETRONICO("Saque no caixa eletrônico Realizado", TipoOperacao.SAIDA),
+    PAGAMENTO_FATURA("Pagamento de Fatura", TipoOperacao.SAIDA),
+    COMPRA_DEBITO("Compra no Débito", TipoOperacao.SAIDA),
+    COMPRA_CREDITO("Compra no Crédito", TipoOperacao.SAIDA),
+    PIX_RECEBIDO("PIX Recebido", TipoOperacao.ENTRADA),
+    PIX_ENVIADO("PIX Enviado", TipoOperacao.SAIDA),
+    PAGAMENTO_PIX("Pagamento no PIX", TipoOperacao.SAIDA );
 
+    private final String descricao;
+    private final TipoOperacao tipoOperacao;
+
+    TipoTransacao(String descricao, TipoOperacao tipoOperacao) {
+        this.descricao = descricao;
+        this.tipoOperacao = tipoOperacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public TipoOperacao getTipoOperacao() {
+        return tipoOperacao;
+    }
 }
